b1ff9f8631444e83d2844c6565f6f4200cb49837
pindellk <pindellk@protonmail.com>
2020-11-18 21:59:05 -0500

    Remove NOT from null lines

--- schema.sql
+++ schema.sql
@@ -6,24 +6,24 @@ USE employees_DB;
 
 CREATE TABLE department (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(30) NOT NULL,
+  name varchar(30) NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id int NOT NULL AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
-  salary decimal NOT NULL,
-  department_id int NOT NULL,
+  title varchar(30) NULL,
+  salary decimal,
+  department_id int,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
   id int NOT NULL AUTO_INCREMENT,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
-  role_id int NOT NULL,
-  manager_id int NOT NULL,
+  first_name varchar(30) NULL,
+  last_name varchar(30) NULL,
+  role_id int,
+  manager_id int NULL,
   PRIMARY KEY (id)
 );
 
